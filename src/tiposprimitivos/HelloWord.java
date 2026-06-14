ee82663d72d23db61a2f60882ea36d409931a10c
JoseAllanAndradedeSantana <allan_santana2810@hotmail.com>
2026-06-14 17:05:25 -0300

    segundo commit

--- src/tiposprimitivos/HelloWord.java
+++ src/tiposprimitivos/HelloWord.java
@@ -3,5 +3,6 @@ package tiposprimitivos;
 public class HelloWord {
     public static void main(String[] args){
         System.out.println("Deus é fiel");
+        System.out.println("curso DevDojo");
     }
 }
