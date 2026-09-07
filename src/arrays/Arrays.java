d4c646dc211737dd6432674d765d4b330deea6a9
JoseAllanAndradedeSantana <allan_santana2810@hotmail.com>
2026-09-07 11:31:48 -0300

    estudos

--- src/arrays/Arrays.java
+++ src/arrays/Arrays.java
@@ -35,5 +35,45 @@ public class Arrays {
             System.out.println(time);
         }
 
+        //1,2,3,4,5 meses
+        //31,28,31,30 dias
+        int[][] dias = new int[3][3];
+        dias[0][0] = 31;
+        dias[0][1] = 28;
+        dias[0][2] = 31;
+
+        dias[1][0] = 30;
+        dias[1][1] = 31;
+        dias[1][2] = 30;
+
+        dias[2][0] = 31;
+        dias[2][1] = 31;
+        dias[2][2] = 31;
+
+        for(int i = 0; i < dias.length; i++){
+            for (int j = 0; j < dias[i].length; j++){
+                System.out.println(dias[i][j]);
+            }
+        }
+
+        for(int[] arrayDia : dias){
+            for (int dia : arrayDia){
+                System.out.println(dia);
+            }
+        }
+
+        int[][] arrayInt = new int[3][];
+        arrayInt[0] = new int [2];
+        arrayInt[1] = new int [4];
+        arrayInt[2] = new int [8];
+
+        for(int[] arrayBase: arrayInt){
+            System.out.println("\n-----------");
+            for(int numero:arrayBase){
+                System.out.print(numero + " ");
+            }
+        }
+
+
     }
 }
